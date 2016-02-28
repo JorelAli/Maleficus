f3038f522db177689535851ad35eb8133db1b58d
Skepter <skepter.misc@gmail.com>
2016-02-28 11:43:07 +0000

    Fix startup bug

--- Plugin - Maleficus/src/io/github/skepter/listeners/PlayerListener.java
+++ Plugin - Maleficus/src/io/github/skepter/listeners/PlayerListener.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.HashMap;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
@@ -16,6 +17,9 @@ import org.bukkit.event.player.PlayerQuitEvent;
 
 import io.github.skepter.Maleficus;
 import io.github.skepter.other.MPlayer;
+import io.github.skepter.tools.MinecraftReflectionUtils;
+import io.github.skepter.tools.PacketBuilder;
+import io.github.skepter.tools.PacketBuilder.PacketType;
 
 public class PlayerListener implements Listener {
 
@@ -57,11 +61,66 @@ public class PlayerListener implements Listener {
 		startClock(event.getPlayer());
 	}
 	
+	
+	
 	public static void startClock(final Player player) {
 		int taskID = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(Maleficus.getInstance(), new Runnable() {
 			@Override
 			public void run() {
 				MPlayer.giveAnima(player, 1);
+				//Calculate the bar things
+				int animaBarSize = 130;
+
+				int maxanima = MPlayer.getLevel(player) * 100;
+				int maxsubanima = MPlayer.getSubLevel(player) * 100;
+				int anima = MPlayer.getAnima(player);
+				int subanima = MPlayer.getSubAnima(player);
+
+				if (anima > maxanima) {
+					anima = maxanima;
+				}
+				if (subanima > maxsubanima) {
+					subanima = maxsubanima;
+				}
+
+				int indicator_anima = 0;
+				int indicator_subanima = 0;
+
+				try {
+					indicator_anima = (anima * animaBarSize) / maxanima;
+					indicator_subanima = (subanima * animaBarSize) / maxsubanima;
+				} catch (ArithmeticException e) {
+					// Do nothing because anima/subanima is already 0
+				}
+
+				int empty_anima = animaBarSize - indicator_anima;
+				int empty_subanima = animaBarSize - indicator_subanima;
+
+				String fullanima = "";
+				for (int i = 0; i < indicator_anima; i++) {
+					fullanima = fullanima + "|";
+				}
+				String fullsubanima = "";
+				for (int i = 0; i < indicator_subanima; i++) {
+					fullsubanima = fullsubanima + "|";
+				}
+				String emptyanima = "";
+				for (int i = 0; i < empty_anima; i++) {
+					emptyanima = emptyanima + "|";
+				}
+				String emptysubanima = "";
+				for (int i = 0; i < empty_subanima; i++) {
+					emptysubanima = emptysubanima + "|";
+				}
+				
+				String animaStr = " §6{§a" + fullanima + ChatColor.GRAY + emptyanima + "§6} §flvl " + MPlayer.getLevel(player);
+				
+				//player.sendMessage(animaStr);
+				try {
+					//new PacketBuilder(player, PacketType.PLAY_OUT_CHAT).set("a", new MinecraftReflectionUtils(player).chatSerialize(animaStr)).set("b", (byte) 2).send();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}, 0, 20);
 		taskIDMap.put(player.getName(), taskID);
